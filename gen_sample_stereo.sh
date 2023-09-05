7a22f85f723825aea1057336ca6d69fd95815550
YaoChengTang <3120215504@bit.edu.cn>
2023-09-05 15:42:58 +0800

    fea(gen): nerf stereo;

--- gen_sample_stereo.sh
+++ gen_sample_stereo.sh
@@ -4,6 +4,10 @@
 # CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raftstereo-sceneflow.pth --sv_root /home/users/chengtang.yao/Output/raftstereo --dataset kitti
 # CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raftstereo-sceneflow.pth --sv_root /home/users/chengtang.yao/Output/raftstereo --dataset eth3d
 
-CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset middlebury_H
-CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset kitti
-CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset eth3d
+# CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset middlebury_H
+# CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset kitti
+# CUDA_VISIBLE_DEVICE=1 python3 gen_sample_stereo.py --restore_ckpt pretrained/raft-stereo-raw.pth --sv_root /home/users/chengtang.yao/Output/raftstereo-raw --dataset eth3d
+
+# CUDA_VISIBLE_DEVICE=2 python3 gen_sample_stereo.py --restore_ckpt pretrained/raftstereo-NS.tar --dataset middlebury_H --sv_root /home/users/chengtang.yao/Output/raftstereo-nerf
+CUDA_VISIBLE_DEVICE=2 python3 gen_sample_stereo.py --restore_ckpt pretrained/raftstereo-NS.tar --dataset kitti --sv_root /home/users/chengtang.yao/Output/raftstereo-nerf
+CUDA_VISIBLE_DEVICE=2 python3 gen_sample_stereo.py --restore_ckpt pretrained/raftstereo-NS.tar --dataset eth3d --sv_root /home/users/chengtang.yao/Output/raftstereo-nerf
