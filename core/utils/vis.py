7cc6fcb422ffea94931870e741a8910a0f367939
YaoChengTang <yao.c.t.adam@gmail.com>
2024-09-23 21:43:03 +0800

    fix(vis): group number and fig size;

--- core/utils/vis.py
+++ core/utils/vis.py
@@ -405,13 +405,13 @@ class Visualizer:
                                        "title": title, 
                                        "cmap" : "gray", },]
             if not in_one_fig:
-                group = cnt // len(img_list)
+                group = cnt // (stop_idx if stop_idx>0 else len(img_list))
                 H,W = img_list[0].shape
                 pre,lat = os.path.splitext(sv_path)
                 sv_path = pre + f"-sequence-{vis_name}" + lat
                 show_imgs(fig_data_list, 
                         sv_img=True, save2where=sv_path, if_inter=False, 
-                        fontsize=20, szWidth=int(group*W/H), szHeight=5, 
+                        fontsize=20, szWidth=np.ceil(W/H)*5, szHeight=5, 
                         group=group, dpi=300)
                 fig_data_list = []
 
