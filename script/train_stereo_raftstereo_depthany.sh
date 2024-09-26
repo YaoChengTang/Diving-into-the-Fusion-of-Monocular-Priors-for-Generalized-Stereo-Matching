1762b9482889e899aed00d7b5c8bf3ccebc89ea9
YaoChengTang <yao.c.t.adam@gmail.com>
2024-09-27 01:03:16 +0800

    update;

--- script/train_stereo_raftstereo_depthany.sh
+++ script/train_stereo_raftstereo_depthany.sh
@@ -22,8 +22,8 @@ export NCCL_P2P_DISABLE=1
 # export NCCL_SOCKET_IFNAME=eth0  # 设置正确的网络接口
 # export MASTER_ADDR=127.0.0.1
 # export MASTER_PORT=29501
-# export CUDA_VISIBLE_DEVICES=0,1,2,3
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 # "/horizon-bucket/saturn_v_dev/01_users/chengtang.yao/Sceneflow"
 # "/horizon-bucket/saturn_v_dev/01_users/chengtang.yao/Middlebury"
